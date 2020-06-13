8ba29895d9ceb90a06af0c9690b8656558bd0b93
Bruce <bruce@redravencomputing.com>
2020-06-13 12:57:21 -0600

    Prepare for recycle view adapter

--- app/src/main/java/com/redravencomputing/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/redravencomputing/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.redravencomputing.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.redravencomputing.coderswag.Adapters.CategoryAdapter
 import com.redravencomputing.coderswag.Model.Category
 import com.redravencomputing.coderswag.R
@@ -19,5 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+   
     }
 }
